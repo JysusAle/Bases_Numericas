c7bf4731e134a90b5dacbcb46f6798c471de4f2f
Tenorio Martinez Jesus Alejandro <tenoriomartinezjesusalejandro@gmail.com>
2025-02-22 22:35:57 -0600

    agrego funcion

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 import random
-""""
+"""""
 Este programa fue realizado por los estudiantes:
 Un conversor de bases numericas
-""""
+"""""
 def bases():
   pass
 
 def menu():
+  pass
+
+def conversor_bases():
   pass
