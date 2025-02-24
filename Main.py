2b5165206c9fb6155340445cbd74aaaadf1fb4e6
arns115 <arnausole184@gmail.com>
2025-02-24 01:52:56 -0600

    Se implemento parte flotante

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def bases():
 
 def menu():
 
-  Arreglo_de_bases = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","S","T"]
+  Arreglo_de_bases = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
   #Definimos la estructura del menu y manejamos errores
   while True:
@@ -21,7 +21,19 @@ def menu():
         print("ERROR: Ingrese un numero entero\n\n")
   
 
-  numero_a_convertir= str(input("\nIngrese El numero a convertir:\t")).strip().upper()
+  numero_a_convertir = str(input("\nIngrese El numero a convertir:\t")).strip().upper()
+  
+  if numero_a_convertir[-1] != ".":
+    numero_a_convertir += "."
+
+  numero_entero_a_convertir = ""
+  numero_flotante_a_convertir = "0"
+
+  for i in range(len(numero_a_convertir)):
+      if numero_a_convertir[i] == ".":
+          numero_entero_a_convertir = numero_a_convertir[:i]
+          numero_flotante_a_convertir = numero_a_convertir[i + 1:len(numero_a_convertir) - 1]
+          break;
 
   while True:
     try:
@@ -33,74 +45,90 @@ def menu():
 
   base_maxima = max(base_destino,base_entrada)
 
-  if base_maxima > 10 + len(Arreglo_de_bases):
+  if base_maxima > len(Arreglo_de_bases):
      
-     print(f"\n La base maxima a convertir es {10 + len(Arreglo_de_bases)}")
+     print(f"\n La base maxima a convertir es {len(Arreglo_de_bases)}")
 
      return 
   
-  elif base_maxima > 10:
-    Arreglo_a_utilizar = Arreglo_de_bases[0:base_maxima-10]
+  Arreglo_a_utilizar = Arreglo_de_bases[0:base_entrada]
+
+
+  parte_entera_convertida = conversor_bases_parte_entera(numero_entero_a_convertir,base_entrada,base_destino,Arreglo_a_utilizar, Arreglo_de_bases)
+  parte_flotante_convertida = conversor_bases_parte_decimal(numero_flotante_a_convertir,base_entrada,base_destino,Arreglo_a_utilizar, Arreglo_de_bases)
+  if parte_entera_convertida == -1 or parte_flotante_convertida == -1:
+      print(f"El numero {numero_a_convertir} no esta en base {base_entrada}")
+      return
   
-  else:
-     Arreglo_a_utilizar = [""]
+  print(f"El numero es {parte_entera_convertida}", end="")
+  if len(set(parte_flotante_convertida)) > 1 or parte_flotante_convertida[0] != "0":
+    print(f".{parte_flotante_convertida}")
 
-  conversor_bases(numero_a_convertir,base_entrada,base_destino,Arreglo_a_utilizar)
 
-
-def conversor_bases(numero_a_convertir, base_entrada, base_destino, Arreglo_a_utilizar):
+def conversor_bases_parte_entera(numero_a_convertir, base_entrada, base_destino, Arreglo_a_utilizar, Arreglo_de_bases):
 
   numero_convertido = ""
   numero_apoyo_en_decimal = 0
   contador_de_exponente = 0
   numero_a_convertir = numero_a_convertir[::-1]
-  residuo = 1
-
-  if base_entrada != 10:
+  residuo = 0
      
-    for valor in numero_a_convertir:
-        try:
-           
-          valor = int(valor)
-
-          numero_apoyo_en_decimal += valor*(base_entrada**(contador_de_exponente))
+  for valor in numero_a_convertir:
+      try:
+          
+          valor_arreglo = Arreglo_a_utilizar.index(valor)
+            
+          numero_apoyo_en_decimal += valor_arreglo*(base_entrada**(contador_de_exponente))
 
           contador_de_exponente += 1
-           
-        except ValueError:
-
-          try:
-             
-            valor_arreglo = 10 + Arreglo_a_utilizar.index(valor)
-             
-            numero_apoyo_en_decimal += valor_arreglo*(base_entrada**(contador_de_exponente))
-
-            contador_de_exponente += 1
+          
+      except ValueError:
         
-          except ValueError:
-             
-             print(f"El numero {numero_a_convertir} no esta en base {base_entrada}")
+            return -1
 
-             return
-  else:
-    numero_apoyo_en_decimal = int(numero_a_convertir)
 
-  if base_destino !=10:
+  while(numero_apoyo_en_decimal!=0):
 
-    while(numero_apoyo_en_decimal!=0):
+    residuo = numero_apoyo_en_decimal % base_destino
 
-      residuo = numero_apoyo_en_decimal - (numero_apoyo_en_decimal//base_destino)*base_destino
+    residuo = Arreglo_de_bases[residuo]
 
-      if residuo >= 10:
-        residuo = Arreglo_a_utilizar[residuo-10]
+    numero_apoyo_en_decimal = numero_apoyo_en_decimal//base_destino
+    
+    numero_convertido += residuo
 
-      numero_apoyo_en_decimal = numero_apoyo_en_decimal//base_destino
-      
-      numero_convertido += str(residuo)
+  return numero_convertido[::-1]
 
-    print(f"\nEl numero es: {numero_convertido[::-1]}")
 
-  else:
-    print(f"\nEl numero es: {numero_apoyo_en_decimal}")
+def conversor_bases_parte_decimal(numero_a_convertir, base_entrada, base_destino, Arreglo_a_utilizar, Arreglo_de_bases):
+
+  numero_convertido = ""
+  numero_apoyo_en_decimal = 0
+  contador_de_exponente = -1
+
+  for valor in numero_a_convertir:
+      try:
+          
+          valor_arreglo = Arreglo_a_utilizar.index(valor)
+            
+          numero_apoyo_en_decimal += valor_arreglo*(base_entrada**(contador_de_exponente))
+
+          contador_de_exponente -= 1
+          
+      except ValueError:
+        
+            return -1
+
+  for potencia_actual in range(-1, -7, -1):
+      print(10 ** (potencia_actual))
+      valor_actual = min(int(numero_apoyo_en_decimal // (base_destino ** (potencia_actual))), base_destino - 1)
+      print(potencia_actual)
+      print(valor_actual)
+      numero_convertido += Arreglo_de_bases[valor_actual]
+      numero_apoyo_en_decimal -= valor_actual * (base_destino ** (potencia_actual))
+
+  return numero_convertido
+    
+
 
 menu()
